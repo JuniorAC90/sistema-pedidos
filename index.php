e419441fa1ed4adfe61c198313d72753d43263d2
Aloizio da Costa Junior <aloizio_junior@hotmail.com>
2021-04-05 10:22:08 -0300

    Criação do cabeçalho

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Pedidos</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/cabecalho.css">
+</head>
+<body>
+    <header class="cabecalho">
+        <h1 class="nome-empresa">Nome da Empresa</h1>
+        <nav class="menu">
+            <a href="/pedidos">Pedidos</a>
+            <a href="/produtos">Produtos</a>
+            <a href="/clientes">Clientes</a>
+        </nav>
+    </header>
+</body>
+</html>
