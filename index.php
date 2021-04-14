56a12cadbf32a88888543f52f50291403a4f0365
Aloizio da Costa Junior <aloizio_junior@hotmail.com>
2021-04-14 09:09:55 -0300

    Criada a estrutura da tabela com valores fictícios como teste de layout

--- index.php
+++ index.php
@@ -7,10 +7,35 @@
     <title>Sistema de Pedidos</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/cabecalho.css">
+    <link rel="stylesheet" href="css/principal.css">
 </head>
 <body>
     <?php
-        include_once "Cabecalho.php";
+        include_once "visao/cabecalho.php"; 
     ?>
+    <main class="container">
+        <table class="tabela">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Cliente</th>
+                    <th>Bairro</th>
+                    <th>Estado</th>
+                    <th>Valor</th>
+                    <th>-</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Nome do Cliente</td>
+                    <td>Nome do Bairro</td>
+                    <td>Nome do Estado</td>
+                    <td>Nome do Valor</td>
+                    <td>-</td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
 </body>
 </html>
